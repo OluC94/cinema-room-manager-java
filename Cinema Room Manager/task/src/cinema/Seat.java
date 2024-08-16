927bae107cb25793facbc5daaf5a3d2664c15f60
OluC94 <coker.olu94@gmail.com>
2024-08-16 13:56:48 +0100

    remove unused features in Seat class

--- Cinema Room Manager/task/src/cinema/Seat.java
+++ Cinema Room Manager/task/src/cinema/Seat.java
@@ -3,19 +3,10 @@ package cinema;
 public class Seat{
     private final Integer row;
     private final Integer column;
-    private boolean isBought = false;
 
     public Seat(Integer row, Integer column){
         this.row = row;
         this.column = column;
-    }
-
-    public void displaySeatData(){
-        System.out.println("Row: " + row + ", Column: " + column);
-    }
-
-    public void buySeat(){
-        isBought = true;
     }
 
     public int getRow(){
